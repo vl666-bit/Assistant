7d65dc66c2ea46456dd042f370d625298cbeda48
vl666-bit <vl@korovainyi.ru>
2025-09-09 13:58:57 +0300

    Update embedding.py

--- app/embedding.py
+++ app/embedding.py
@@ -1,1 +1,7 @@
+from sentence_transformers import SentenceTransformer
+from app.config import EMBED_MODEL
 
+model = SentenceTransformer(EMBED_MODEL)
+
+def get_embeddings(texts: list[str]) -> list[list[float]]:
+    return model.encode(texts, convert_to_numpy=True).tolist()

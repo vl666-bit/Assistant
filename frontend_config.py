d012d1287d1d7f5a37dd07b49fd93fcbab72305a
vl666-bit <vl@korovainyi.ru>
2025-11-07 14:34:33 +0300

    Create frontend_config.py

---
+++ frontend_config.py
@@ -0,0 +1,8 @@
+# frontend_config.py
+
+# Базовый URL Confluence (для ссылок в UI)
+CONFLUENCE_DOMAIN = "atlassian.net"
+BASE_URL = f"https://{CONFLUENCE_DOMAIN}/wiki"
+
+# Базовый URL API (FastAPI)
+API_URL = "http://127.0.0.1:8000"

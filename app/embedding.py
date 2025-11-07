78cd8a7ce319ff8443036d3a3495f76322259507
vl666-bit <vl@korovainyi.ru>
2025-11-07 14:16:22 +0300

    Delete app/embedding.py

--- app/embedding.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-from app.config import EMBED_MODEL
-
-model = SentenceTransformer(EMBED_MODEL)
-
-def get_embeddings(texts: list[str]) -> list[list[float]]:
-    return model.encode(texts, convert_to_numpy=True).tolist()
